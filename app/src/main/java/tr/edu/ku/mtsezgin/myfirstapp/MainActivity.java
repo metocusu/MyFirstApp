9c46668f2a56b20956af4bbe83d17d3204950803
mtsezgin <metocusu@gmail.com>
2018-02-12 16:54:35 +0300

    Resources revised.

--- app/src/main/java/tr/edu/ku/mtsezgin/myfirstapp/MainActivity.java
+++ app/src/main/java/tr/edu/ku/mtsezgin/myfirstapp/MainActivity.java
@@ -1,8 +1,8 @@
 package tr.edu.ku.mtsezgin.myfirstapp;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
 
