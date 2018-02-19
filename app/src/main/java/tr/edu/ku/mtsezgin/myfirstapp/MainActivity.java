a558f96d1b954be5a7c185678fc9955bd90918f4
mtsezgin <metocusu@gmail.com>
2018-02-19 14:25:06 +0300

    Resources revised.

--- app/src/main/java/tr/edu/ku/mtsezgin/myfirstapp/MainActivity.java
+++ app/src/main/java/tr/edu/ku/mtsezgin/myfirstapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
